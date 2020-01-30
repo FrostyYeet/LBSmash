0db11da8938c68564e77ecb1fdd2050c6fc8113a
FrostyYeet <45594578+FrostyYeet@users.noreply.github.com>
2020-01-30 15:12:23 +0100

    Characters
    Added a simple template to create a new character. Also added two different versions(Heavy and Light) of it

---
+++ LBSmash/Assets/Scripts/CharacterScripts/Light.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Light", menuName = "Characters/Light")]
+public class Light : Character
+{
+    [HideInInspector] public float Stamina;
+
+    public Image icon;
+}
