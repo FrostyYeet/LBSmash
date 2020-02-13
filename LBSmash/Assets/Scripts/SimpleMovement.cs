a37b910c2336c6a2634c87c9302a7ec4746f9150
FrostyYeet <45594578+FrostyYeet@users.noreply.github.com>
2020-02-13 14:53:43 +0100

    Oliver

--- LBSmash/Assets/Scripts/SimpleMovement.cs
+++ LBSmash/Assets/Scripts/SimpleMovement.cs
@@ -6,10 +6,18 @@ public class SimpleMovement : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    public int jumpCount = 0;
+    public int maxJumps = 2;
 
     public KeyCode left;
     public KeyCode right;
     public KeyCode jump;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask WhatIsGround;
+
+    public bool isGrounded;
 
     Rigidbody2D rb;
 
@@ -20,20 +28,23 @@ public class SimpleMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(left))
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, WhatIsGround);
+
+        rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb.velocity.y);
+
+        if (Physics2D.Raycast(transform.position, -Vector2.up, gameObject.GetComponent<Collider2D>().bounds.size.y / 2 + 0.1f, WhatIsGround))
         {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-        } else if (Input.GetKey(right))
-        {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
+            isGrounded = true;
+            jumpCount = 1;
         }
         else
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
+            isGrounded = false;
         }
-        
-        if (Input.GetKeyDown(jump))
+
+        if (Input.GetKeyDown(jump) && jumpCount < maxJumps) 
         {
+            jumpCount++;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
